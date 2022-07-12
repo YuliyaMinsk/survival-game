a38ddcbe81f6dd480560275f9590dec89c6ffbdb
Yuliya <yuliya.rostova@gmail.com>
2022-07-12 18:42:07 +0300

    feat: launch empty game window  by phaser

---
+++ src/js/survival-game.js
@@ -0,0 +1,29 @@
+const config = {
+  width: 512,
+  height: 512,
+  backgroundColor: '#333333',
+  type: Phaser.AUTO,
+  parent: 'survival-game',
+  scene: [],
+  scale: {
+    zoom: 2
+  },
+  physics: {
+    default: 'matter',
+    matter: {
+      debug: true,
+      gravity: { y: 0 }
+    }
+  },
+  plugins: {
+    scene: [
+      {
+        plugin: PhaserMatterCollisionPlugin.default,
+        key: 'matterCollision',
+        mapping: 'matterCollision'
+      }
+    ]
+  }
+};
+
+new Phaser.Game(config);
