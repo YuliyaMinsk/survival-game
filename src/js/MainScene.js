1ab342a12172ac75fc888170cf4b1a0442e88227
Yuliya <yuliya.rostova@gmail.com>
2022-07-13 10:39:31 +0300

    feat: add basic movement

---
+++ src/js/MainScene.js
@@ -0,0 +1,42 @@
+export default class MainScene extends Phaser.Scene {
+  constructor() {
+    super('MainScene');
+  }
+
+  preload() {
+    console.log('preload');
+  }
+
+  create() {
+    console.log('create');
+    this.player = new Phaser.Physics.Matter.Sprite(this.matter.world);
+    this.inputKeys = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+    });
+  }
+
+  update() {
+    console.log('update');
+    const speed = 2.5;
+    let playerVelocity = new Phaser.Math.Vector2();
+
+    if (this.inputKeys.left.isDown) {
+      playerVelocity.x = -1;
+    } else if (this.inputKeys.right.isDown) {
+      playerVelocity.x = 1;
+    }
+
+    if (this.inputKeys.up.isDown) {
+      playerVelocity.y = -1;
+    } else if (this.inputKeys.down.isDown) {
+      playerVelocity.y = 1;
+    }
+
+    playerVelocity.normalize();
+    playerVelocity.scale(speed);
+    this.player.setVelocity(playerVelocity.x, playerVelocity.y);
+  }
+}
--- src/js/survival-game.js
+++ src/js/survival-game.js
@@ -1,29 +1,31 @@
+import MainScene from './MainScene.js';
+
 const config = {
   width: 512,
   height: 512,
   backgroundColor: '#333333',
   type: Phaser.AUTO,
   parent: 'survival-game',
-  scene: [],
+  scene: [MainScene],
   scale: {
-    zoom: 2
+    zoom: 2,
   },
   physics: {
     default: 'matter',
     matter: {
       debug: true,
-      gravity: { y: 0 }
-    }
+      gravity: { y: 0 },
+    },
   },
   plugins: {
     scene: [
       {
         plugin: PhaserMatterCollisionPlugin.default,
         key: 'matterCollision',
-        mapping: 'matterCollision'
-      }
-    ]
-  }
+        mapping: 'matterCollision',
+      },
+    ],
+  },
 };
 
 new Phaser.Game(config);
